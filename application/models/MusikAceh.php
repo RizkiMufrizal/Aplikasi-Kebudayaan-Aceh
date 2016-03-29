28102ad53dcdf093d4b385dabbe0c16414849b23
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-29 23:09:57 +0700

    tambah model musik aceh

---
+++ application/models/MusikAceh.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Mar 29, 2016
+ * Time 10:42:57 PM
+ * Encoding UTF-8
+ * Project Aplikasi-Kebudayaan-Aceh
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class MusikAceh extends CI_Model {
+
+    public function ambilJumlahMusikAceh() {
+        return $this->db->count_all_results('tb_musik_aceh', FALSE);
+    }
+
+    public function ambilMusikAceh($page, $size) {
+        return $this->db->get('tb_kuliner-aceh', $size, $page);
+    }
+
+    public function simpanMusikAceh($musikAceh) {
+        $this->db->insert('tb_musik_aceh', $musikAceh);
+    }
+
+    public function ubahMusikAceh($musikAceh, $idMusikAceh) {
+        $this->db->where('id_musik_aceh', $idMusikAceh);
+        $this->db->update('tb_musik_aceh', $musikAceh);
+    }
+
+    public function hapusMusikAceh($idMusikAceh) {
+        $musikAceh = array(
+            'id_musik_aceh' => $idMusikAceh
+        );
+        $this->db->delete('tb_musik_aceh', $musikAceh);
+    }
+
+}
