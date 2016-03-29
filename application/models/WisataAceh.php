0a7d3129f2fb6a3245bd34d5d961ab18a228c0d3
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-29 23:10:14 +0700

    tambah model wisata aceh

---
+++ application/models/WisataAceh.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Mar 29, 2016
+ * Time 10:43:13 PM
+ * Encoding UTF-8
+ * Project Aplikasi-Kebudayaan-Aceh
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class WisataAceh extends CI_Model {
+
+    public function ambilJumlahWisataAceh() {
+        return $this->db->count_all_results('tb_wisata_aceh', FALSE);
+    }
+
+    public function ambilWisataAceh($page, $size) {
+        return $this->db->get('tb_kuliner-aceh', $size, $page);
+    }
+
+    public function simpanWisataAceh($wisataAceh) {
+        $this->db->insert('tb_wisata_aceh', $wisataAceh);
+    }
+
+    public function ubahWisataAceh($wisataAceh, $idWisataAceh) {
+        $this->db->where('id_wisata_aceh', $idWisataAceh);
+        $this->db->update('tb_wisata_aceh', $wisataAceh);
+    }
+
+    public function hapusWisataAceh($idWisataAceh) {
+        $wisataAceh = array(
+            'id_wisata_aceh' => $idWisataAceh
+        );
+        $this->db->delete('tb_wisata_aceh', $wisataAceh);
+    }
+
+}
