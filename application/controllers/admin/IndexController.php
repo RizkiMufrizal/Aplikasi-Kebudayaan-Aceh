88995db55144b7925f945c802a4af1e4fdf5e903
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-30 22:15:38 +0700

    tambah index controller

---
+++ application/controllers/admin/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Mar 30, 2016
+ * Time 7:46:39 PM
+ * Encoding UTF-8
+ * Project Aplikasi-Kebudayaan-Aceh
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class IndexController extends CI_Controller {
+
+    public function index() {
+        $this->load->view('admin/IndexView');
+    }
+
+}
