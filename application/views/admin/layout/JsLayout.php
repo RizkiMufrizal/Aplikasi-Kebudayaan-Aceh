84a4106e91977ed2667ae38d15f077f7821ec963
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-31 16:44:41 +0700

    tambah js layout

---
+++ application/views/admin/layout/JsLayout.php
@@ -0,0 +1,11 @@
+<!--
+
+ Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ Since Mar 31, 2016
+ Time 4:30:57 PM
+ Encoding UTF-8
+ Project Aplikasi-Kebudayaan-Aceh
+ Package Expression package is undefined on line 9, column 12 in Templates/Scripting/EmptyPHPWebPage.php.
+  
+-->
+<script src="<?php echo base_url(); ?>assets/js/bundle.min.js" type="text/javascript"></script>
