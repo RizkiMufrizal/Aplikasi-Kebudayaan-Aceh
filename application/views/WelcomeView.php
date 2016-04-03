e9dc65bfd85dd75623c99f45a2a1bd987f9550f8
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-03 11:17:27 +0700

    update welcome view

---
+++ application/views/WelcomeView.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<!--
+
+ Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ Since Apr 3, 2016
+ Time 10:11:35 AM
+ Encoding UTF-8
+ Project Aplikasi-Kebudayaan-Aceh
+ Package Expression package is undefined on line 9, column 12 in Templates/Scripting/EmptyPHPWebPage.php.
+  
+-->
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+        <title>Parallax Template - Materialize</title>
+
+        <!-- CSS  -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>assets/css/bundle-user.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <link href="<?php echo base_url(); ?>assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    </head>
+    <body>
+        <nav class="green z-depth-2" role="navigation">
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="#" class="brand-logo">
+                    <img  style="width: 150px; width: 150px" src="<?php echo base_url(); ?>assets/img/logo.png" />
+                </a>
+                <ul class="right hide-on-med-and-down">
+                    <li>
+                        <a class="white-text" href="#">Kuliner</a>
+                    </li>
+                    <li>
+                        <a class="white-text" href="#">Wisata</a>
+                    </li>
+                    <li>
+                        <a class="white-text" href="#">Tarian</a>
+                    </li>
+                    <li>
+                        <a class="white-text" href="#">Musik</a>
+                    </li>
+                </ul>
+
+                <ul id="nav-mobile" class="side-nav">
+                    <li>
+                        <a href="#">Kuliner</a>
+                    </li>
+                    <li>
+                        <a href="#">Wisata</a>
+                    </li>
+                    <li>
+                        <a href="#">Tarian</a>
+                    </li>
+                    <li>
+                        <a href="#">Musik</a>
+                    </li>
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+
+        <div id="index-banner" class="parallax-container">
+            <div class="section no-pad-bot">
+                <div class="container">
+                    <br><br>
+                    <h1 class="header center text-lighten-2">Selamat Datang</h1>
+                    <div class="row center">
+                        <h4 class="header col s12 light">
+                            Aplikasi Kebudayaan Aceh
+                        </h4>
+                    </div>
+                </div>
+            </div>
+            <div class="parallax"><img src="<?php echo base_url(); ?>assets/img/MesjidRaya.jpg" alt="Unsplashed background img 1"></div>
+        </div>
+
+
+        <div class="container">
+            <div class="section">
+
+                <!--   Icon Section   -->
+                <div class="row">
+                    <div class="col s12 center">
+                        <div class="icon-block">
+                            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+                            <h5 class="center">About Project</h5>
+
+                            <p class="light">
+                                Aplikasi ini dibuat dalam rangka membudidayakan kebudayaan aceh
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="parallax-container valign-wrapper">
+            <div class="parallax"><img src="<?php echo base_url(); ?>assets/img/MuseumTsunamiAceh.jpg" alt="Unsplashed background img 2"></div>
+        </div>
+
+        <footer class="page-footer teal">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">About Me</h5>
+                        <p class="grey-text text-lighten-4">
+                            Developer bernama Rizki Mufrizal, berasal dari aceh. Sekarang developer sebagai mahasiswa aktif jurusan teknik informatika di Universitas Gunadarma. Di sela - sela kesibukan sebagai asisten laboratorium teknik informatika, developer juga aktif dalam mempelajari berbagai teknologi open source terutama di bidang pemrograman Java, Javascript dan Linux.
+                        </p>
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text">Contact Us</h5>
+                        <ul>
+                            <li>
+                                <a class="white-text" href="#!">Link 1</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                </div>
+            </div>
+        </footer>
+
+        <!--  Scripts-->
+        <script src="<?php echo base_url(); ?>assets/js/bundle-user.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/init.js"></script>
+
+    </body>
+</html>
