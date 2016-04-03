55a23fe87a5e96afa2a533565f956cd1b8ac6120
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-03 11:17:13 +0700

    update gulp untuk materilizecss

--- gulpfile.js
+++ gulpfile.js
@@ -22,23 +22,45 @@ gulp.task('minify-js', function() {
       './bower_components/jquery/dist/jquery.min.js',
       './bower_components/bootstrap/dist/js/bootstrap.min.js',
       './bower_components/datatables.net/js/jquery.dataTables.min.js',
-      './bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js',
-      './bower_components/angular/angular.min.js',
-      './bower_components/angular-animate/angular-animate.min.js',
-      './bower_components/angular-loading-bar/build/loading-bar.min.js',
-      './bower_components/angular-ui-router/release/angular-ui-router.min.js',
-      './bower_components/oclazyload/dist/ocLazyLoad.min.js'
+      './bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js'
     ])
     .pipe(gulpConcat('bundle.min.js'))
     .pipe(gulpUglify())
     .pipe(gulp.dest('./assets/js/'));
 });
 
+gulp.task('minify-js-user', function() {
+  gulp
+    .src([
+      './bower_components/jquery/dist/jquery.min.js',
+      './bower_components/materialize.js',
+      './bower_components/angular/angular.min.js',
+      './bower_components/angular-animate/angular-animate.min.js',
+      './bower_components/angular-loading-bar/build/loading-bar.min.js',
+      './bower_components/angular-ui-router/release/angular-ui-router.min.js',
+      './bower_components/oclazyload/dist/ocLazyLoad.min.js'
+    ])
+    .pipe(gulpConcat('bundle-user.min.js'))
+    .pipe(gulpUglify())
+    .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('minify-css-user', function() {
+  gulp.src([
+    './bower_components/materialize.css',
+    './bower_components/angular-loading-bar/build/loading-bar.min.css'
+  ])
+    .pipe(gulpConcat('bundle-user.min.css'))
+    .pipe(gulpMinifyCss({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('./assets/css/'));
+});
+
 gulp.task('minify-css', function() {
   gulp.src([
     './bower_components/bootstrap/dist/css/bootstrap.min.css',
-    './bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css',
-    './bower_components/angular-loading-bar/build/loading-bar.min.css'
+    './bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'
   ])
     .pipe(gulpConcat('bundle.min.css'))
     .pipe(gulpMinifyCss({
@@ -56,4 +78,16 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('./assets/fonts/'));
 });
 
-gulp.task('default', ['minify-css', 'minify-js', 'fonts']);
+gulp.task('fonts-user', function() {
+  gulp.src([
+    './bower_components/fonts/roboto/Roboto-Regular.woff2',
+    './bower_components/fonts/roboto/Roboto-Regular.woff',
+    './bower_components/fonts/roboto/Roboto-Regular.ttf',
+    './bower_components/fonts/roboto/Roboto-Light.woff2',
+    './bower_components/fonts/roboto/Roboto-Light.woff',
+    './bower_components/fonts/roboto/Roboto-Light.ttf'
+  ])
+    .pipe(gulp.dest('./assets/fonts/roboto/'));
+});
+
+gulp.task('default', ['minify-css', 'minify-js', 'fonts', 'minify-css-user', 'minify-js-user', 'fonts-user']);
